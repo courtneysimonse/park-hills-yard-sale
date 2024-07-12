fbe581a8bffb6a99bd4068c36cf9718d0f5cc9bd
Courtney Simonse <courtney@artemismaps.com>
2024-07-12 11:16:07 -0400

    add yard sale markers

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 // US map options
 var options = {
   zoomSnap: .5,
-  center: [39, -97],
-  zoom: 5,
-  minZoom: 2,
+  center: [39.07,	-84.53],
+  zoom: 16,
+  minZoom: 12,
   zoomControl: false,
   // attributionControl: false
 }
@@ -13,129 +14,72 @@ var options = {
 var map = L.map('mapid', options);
 
 // request tiles and add to map
-// https://leaflet-extras.github.io/leaflet-providers/preview/
-var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-	maxZoom: 19,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
 
 // Stadia
-// var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
-// 	maxZoom: 20,
-// 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-// });
-//
-// var Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-// 	maxZoom: 20,
-// 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-// });
-//
-// var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-// 	maxZoom: 20,
-// 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-// });
-
-// change zoom control position
-L.control.zoom({
-  position: 'bottomleft'
+var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// setup slider
-var dateSlider = document.getElementById('slider');
-
-function timestamp(str) {
-    return new Date(str).getTime();
-}
-
-noUiSlider.create(dateSlider, {
-// Create two timestamps to define a range.
-    range: {
-        min: timestamp('2010'),
-        max: timestamp('2016')
-    },
-
-// Steps of one week
-    step: 7 * 24 * 60 * 60 * 1000,
-
-// Two more timestamps indicate the handle starting positions.
-    start: [timestamp('2011-01-15')],
-
-// No decimals
-    format: {
-      to: function (value) {
-        return value;
-      },
-      from: function (value) {
-        return Number(value);
-      }
-    }
-});
-
-
-// update slider
-var dateValues = [
-    document.getElementById('event')
-];
-
-var formatter = new Intl.DateTimeFormat('en-US');
-
-dateSlider.noUiSlider.on('update', function (values, handle) {
-    dateValues[handle].innerHTML = formatter.format(new Date(+values[handle]));
-});
-
+// // change zoom control position
+// L.control.zoom({
+//   position: 'topright'
+// }).addTo(map);
 
 
 // GET DATA
-processData();
+await processData();
 
 // PROCESS DATA FUNCTION
-function processData() {
+async function processData() {
 
-  drawMap();
+  // https://docs.google.com/spreadsheets/d/1xF5YkQlI-DKL1OD9vIcyJHbw-GLsmHcSIIawUehH5OQ/edit?usp=sharing
+  const features = await d3.csv('https://docs.google.com/spreadsheets/d/1xF5YkQlI-DKL1OD9vIcyJHbw-GLsmHcSIIawUehH5OQ/gviz/tq?tqx=out:csv&sheet=addresses', (d) => {
+    // console.log(d);
+    if (d.lat != "") {
+      return {
+        type: "Feature",
+        properties: {
+          address: d.address,
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [+d.lon,+d.lat]
+        }
+      }
+    }
 
-  // example breaks for legend
-  var breaks = [1, 4, 7, 10];
-  var colorize = chroma.scale(chroma.brewer.BuGn).classes(breaks).mode('lab');
+  })
 
-  drawLegend(breaks, colorize);
+
+  drawMap(features);
 
 }   //end processData()
 
 // DRAW MAP FUNCTION
-function drawMap() {
+function drawMap(data) {
+
+  const bounds = L.latLngBounds([39.07,	-84.531]);
+
+  const layer = L.geoJSON(data, {
+    pointToLayer: function(geoJsonPoint, latlng) {
+      bounds.extend(latlng)
+      return L.marker(latlng, {
+        riseOnHover: true
+      });
+    }
+    }).bindTooltip(function (layer) {
+      return layer.feature.properties.address;
+    }).bindPopup(function (layer) {
+        return layer.feature.properties.address;
+    }).addTo(map);
+  
+    bounds.pad(0.1);
+
+    map.fitBounds(bounds);
+
+    layer.on('click', (e) => {
+      map.flyTo(e.latlng, 18)
+    })
 
 }   //end drawMap()
-
-function drawLegend(breaks, colorize) {
-
-  var legendControl = L.control({
-    position: 'topright'
-  });
-
-  legendControl.onAdd = function(map) {
-
-    var legend = L.DomUtil.create('div', 'legend');
-    return legend;
-
-  };
-
-  legendControl.addTo(map);
-
-  var legend = document.querySelector('.legend');
-  var legendHTML = "<h3><span>YYYY</span> Legend</h3><ul>";
-
-  for (var i = 0; i < breaks.length - 1; i++) {
-
-    var color = colorize(breaks[i], breaks);
-
-    var classRange = '<li><span style="background:' + color + '"></span> ' +
-        breaks[i].toLocaleString() + ' &mdash; ' +
-        breaks[i + 1].toLocaleString() + '</li>'
-    legendHTML += classRange;
-
-  }
-
-  legendHTML += '</ul><p>(Data from SOURCE)</p>';
-  legend.innerHTML = legendHTML;
-
-} // end drawLegend()
